64d9037b931ade44f9fa11a4e747c99364fc16ff
kimberly banegas <kimberlymanzanares998>
2026-03-24 11:38:52 -0600

    inicio proyecto

---
+++ Telefonia.java
@@ -0,0 +1,9 @@
+ class Telefono{
+    public String forma;
+    private Integer bateria;
+    protected String camara;
+    public void vender () {
+    }
+    private void reproducirVideo () {
+    }
+}
