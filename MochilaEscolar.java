f0b9b7637d9a280a2f6b31c0d645259eb6eb05d0
kimberly banegas <kimberlymanzanares998>
2026-09-02 20:18:43 -0600

    mochila

---
+++ MochilaEscolar.java
@@ -0,0 +1,24 @@
+public class MochilaEscolar {
+    public String color;
+    public String marca;
+    public Double tamaño;
+    public Integer capacidad;
+
+    public MochilaEscolar(String nombredelaclase){
+
+        // nombre=nombredelaclase;
+        capacidad=30;
+
+    System.out.print("color, marca, tamaño, capacidad");
+     System.out.println(nombredelaclase);
+
+       
+    }
+
+    public void llevar() {
+    }
+
+    public void guardar() {
+    }
+
+}
