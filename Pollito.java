a6964be74822b0b2bf6cc1db70a1105fc29c134c
kimberly banegas <kimberlymanzanares998>
2026-09-02 20:21:41 -0600

    pollito

---
+++ Pollito.java
@@ -0,0 +1,12 @@
+public class Pollito extends MascotaVirtual {
+     public Pollito (String nombreElegido){
+        super (nombreElegido);
+        
+     }
+      @Override
+      public void hacerSonido(){
+      System.out.println(nombre + "dice: ¡Pio, Pio!");
+
+}
+}
+
